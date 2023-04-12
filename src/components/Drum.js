a1bdec46f276e1372f3a6aae02a09189ede487ea
richardsonqiu <richardsonqiu@gmail.com>
2023-04-12 22:34:49 +0800

    Change global event listener to handleKeyDown

--- src/components/Drum.js
+++ src/components/Drum.js
@@ -2,25 +2,21 @@ import DrumPad from "./DrumPad";
 import React, { useEffect } from 'react';
 
 function Drum({ kit }) {
-  useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown);
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [kit]);
-
   const handleKeyDown = (e) => {
+    console.log(e);
     const pad = kit.kitPads.find((pad) => pad.padKey.includes(e.key));
     if (pad) {
       const audio = new Audio(pad.padAudioSrc);
       console.log(pad.padAudioSrc);
       audio.currentTime = 0.05;
-      audio.play()
+      audio.play();
     }
   }
 
+  if (!kit) return;
+
   return (
-    <div>
+    <div style={{backgroundColor: "#555"}} tabIndex={0} onKeyDown={handleKeyDown}>
       {kit.kitName}
       {
         kit.kitPads.map((pad) => (
